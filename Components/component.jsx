36a489de5738153b0bb594c335f7f83542b3dead
Adam <adambacon1@hotmail.com>
2020-02-06 19:22:40 +0000

    JSX file

---
+++ Components/component.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import ReactDOM from "react-dom";
+import Chart from "react-google-charts";
+
+const columns = [
+  { type: "string", id: "President" },
+  { type: "date", id: "Start" },
+  { type: "date", id: "End" }
+];
+
+class <%=$PLASTER_PARAM_ControlName%> extends React.Component {
+  render() {
+    const options = {
+      backgroundColor: this.props.backgroundColor,
+      colors: this.props.colors,
+      timeline: {
+        showRowLabels: this.props.showRowLabels,
+        showRowNumber: this.props.showRowNumber,
+        groupByRowLabel: this.props.groupByRowLabel,
+        colorByRowLabel: this.props.colorByRowLabel,
+        rowLabelStyle: {
+          fontName: this.props.fontName,
+          fontSize: this.props.fontSize,
+          color: this.props.fontColor,
+        },
+        barLabelStyle: { fontName: this.props.fontName, fontSize: 14 },
+      }
+    }
+    return (
+      <div className="App">
+        <Chart
+          chartType="Timeline"
+          data={[columns, ...this.props.data]}
+          width={this.props.width}
+          height={this.props.height}
+          options={options}
+        />
+      </div>
+    )
+  }
+}
+
+export default <%=$PLASTER_PARAM_ControlName %>
